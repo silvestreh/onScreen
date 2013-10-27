5540f2be6883be07fac2bb1e646118fcf7f9719b
Silvestre Herrera <silvestre.herrera@gmail.com>
2013-10-27 01:57:26 -0300

    no php :(

--- header.php
+++
@@ -1,7 +0,0 @@
-<ul class="menu">
-  <li class="home"><a href="index.php"><i class="fa fa-home"></i></a></li>
-  <li><a href="?demo=appenditems"><i class="fa fa-plus-circle"></i>Append new elements</a></li>
-  <li><a href="?demo=lazy"><i class="fa fa-picture-o"></i>Lazy load of images</a></li>
-  <li><a href="?demo=horizontal"><i class="fa fa-resize-horizontal"></i>Horizontal scrolling</a></li>
-  <li class="fork"><a href="http://github.com/silvestreh/onScreen/fork"><i class="fa fa-github"></i>Fork me</a></li>
-</ul>
