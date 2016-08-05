106757303f04bde75155f9c9e1c360d24be4c19c
Silvestre Herrera <silvestre.herrera@gmail.com>
2016-08-05 00:19:31 -0300

    Small refactors to make testing easier

--- test/observe-dom_test.js
+++ test/observe-dom_test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import observeDOM from '../lib/helpers/observe-dom';
 
-describe('DOM observer', () => {
+describe('DOM observer with MutationObserver', () => {
     let DOMChanged;
     let div;
 
